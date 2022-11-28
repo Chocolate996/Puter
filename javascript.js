234dd4dabe84bdb38b558606529a1fe2b84732a8
Chocolate <78719519+Chocolate996@users.noreply.github.com>
2022-11-28 10:35:12 +0800

    update

--- javascript.js
+++ javascript.js
@@ -1,19 +1,38 @@
+//nav高度
 var get_nav_h = document.querySelector('.nav');
 var out_navo_h_o = (getComputedStyle(get_nav_h).height);
-var out_navo_h = parseInt(out_navo_h_o) + parseInt('30') +'px';
-console.log(out_navo_h_o)
-console.log(out_navo_h)
+var out_navo_h = parseInt(out_navo_h_o) + parseInt('30') + 'px';
 
 document.getElementById('nav-o').style.height = (out_navo_h)
 
-var get_h = document.querySelector('.music-margin');
+
+//mark样式
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate')
+      observer.unobserve(entry.target)
+    }
+  })
+})
+
+document.querySelectorAll('mark').forEach(mark => {
+  observer.observe(mark)
+})
+
+
+//music长宽
+var get_h = document.querySelector('.download-margin');
 var out_w = (getComputedStyle(get_h).height);
 
 let arr = document.getElementsByClassName('out-w');
 for (let i = 0, len = arr.length; i < len; i++) {
-    document.getElementsByClassName('out-w')[i].style.width = (out_w)
+  document.getElementsByClassName('out-w')[i].style.width = (out_w)
+  document.getElementsByClassName('out-w')[i].style.height = (out_w)
 }
-console.log(get_h)
-console.log(out_w)
-console.log(out_h)
 
+let arr1 = document.getElementsByClassName('egg');
+for (let i = 0, len = arr1.length; i < len; i++) {
+  document.getElementsByClassName('egg')[i].style.width = (out_w)
+  document.getElementsByClassName('egg')[i].style.height = (out_w)
+}
